5a3ce59b5c38a2e4b1fed0bfaf35172a66a68ee5
Rohan Prinja <rohan.prinja@gmail.com>
2023-06-01 17:24:16 -0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,19 +1,23 @@
-def react(string, ignore=None):
+# python3 5.py < 5.txt
+with open(0) as file:
+    data = file.read().strip()
+
+def react(s):
     stack = []
-    for char in string:
-        if char.lower() == ignore:
-            continue
-        if not stack:
-            stack.append(char)
-            continue
-        if stack[-1].lower() == char.lower() and stack[-1] != char:
+    for char in s:
+        if stack and abs(ord(char) - ord(stack[-1])) == 32:
             stack.pop()
-            continue
-        stack.append(char)
+        else:
+            stack.append(char)
     return len(stack)
 
-with open('5.txt', 'r') as f:
-    string = f.read().strip()
-    print(react(string))
-    candidate_chars = set(char.lower() for char in string)
-    print(min(react(string, candidate_char) for candidate_char in candidate_chars))
+def best_react(s):
+    types = set(c.lower() for c in s)
+    result = 1e10
+    for char in types:
+        candidate = react(c for c in s if c.lower() != char)
+        result = min(result, candidate)
+    return result
+
+print(react(data))
+print(best_react(data))
